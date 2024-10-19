33ed403647e9249a3ec59a465fec4237021815c3
Alex Kotenko <alexykot@gmail.com>
2024-10-19 14:02:35 +0100

    Add AllRepeat function

--- combinations_test.go
+++ combinations_test.go
@@ -264,3 +264,202 @@ func TestStringCombinationsN(t *testing.T) {
 		})
 	}
 }
+
+func TestAllWithRepetitions(t *testing.T) {
+	tt := []struct {
+		name string
+		in   []string
+		m    int
+		out  [][]string
+	}{
+		{
+			name: "Empty slice",
+			in:   []string{},
+			m:    1,
+			out:  nil,
+		},
+		{
+			name: "Single item, m = 1",
+			in:   []string{"A"},
+			m:    1,
+			out: [][]string{
+				{"A"},
+			},
+		},
+		{
+			name: "Single item, m = 2",
+			in:   []string{"A"},
+			m:    2,
+			out: [][]string{
+				{"A"},
+				{"A", "A"},
+			},
+		},
+		{
+			name: "Two items, m = 1",
+			in:   []string{"A", "B"},
+			m:    1,
+			out: [][]string{
+				{"A"},
+				{"B"},
+			},
+		},
+		{
+			name: "Two items, m = 2",
+			in:   []string{"A", "B"},
+			m:    2,
+			out: [][]string{
+				{"A"},
+				{"B"},
+				{"A", "A"},
+				{"A", "B"},
+				{"B", "A"},
+				{"B", "B"},
+			},
+		},
+		{
+			name: "Three items, m = 2",
+			in:   []string{"A", "B", "C"},
+			m:    2,
+			out: [][]string{
+				{"A"},
+				{"B"},
+				{"C"},
+				{"A", "A"},
+				{"A", "B"},
+				{"A", "C"},
+				{"B", "A"},
+				{"B", "B"},
+				{"B", "C"},
+				{"C", "A"},
+				{"C", "B"},
+				{"C", "C"},
+			},
+		},
+		{
+			name: "Two items, m = 3",
+			in:   []string{"A", "B"},
+			m:    3,
+			out: [][]string{
+				{"A"},
+				{"B"},
+				{"A", "A"},
+				{"A", "B"},
+				{"B", "A"},
+				{"B", "B"},
+				{"A", "A", "A"},
+				{"A", "A", "B"},
+				{"A", "B", "A"},
+				{"A", "B", "B"},
+				{"B", "A", "A"},
+				{"B", "A", "B"},
+				{"B", "B", "A"},
+				{"B", "B", "B"},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			out := AllRepeat(tc.in, tc.m)
+			if !reflect.DeepEqual(out, tc.out) {
+				t.Errorf("error: \nreturn:\t%v\nwant:\t%v", out, tc.out)
+			}
+		})
+	}
+}
+
+func TestAllWithRepetitionsInt(t *testing.T) {
+	tt := []struct {
+		name string
+		in   []int
+		m    int
+		out  [][]int
+	}{
+		{
+			name: "Empty slice",
+			in:   []int{},
+			m:    1,
+			out:  nil,
+		},
+		{
+			name: "Single item, m = 1",
+			in:   []int{1},
+			m:    1,
+			out: [][]int{
+				{1},
+			},
+		},
+		{
+			name: "Single item, m = 2",
+			in:   []int{1},
+			m:    2,
+			out: [][]int{
+				{1},
+				{1, 1},
+			},
+		},
+		{
+			name: "Two items, m = 2",
+			in:   []int{1, 2},
+			m:    2,
+			out: [][]int{
+				{1},
+				{2},
+				{1, 1},
+				{1, 2},
+				{2, 1},
+				{2, 2},
+			},
+		},
+		{
+			name: "Three items, m = 2",
+			in:   []int{1, 2, 3},
+			m:    2,
+			out: [][]int{
+				{1},
+				{2},
+				{3},
+				{1, 1},
+				{1, 2},
+				{1, 3},
+				{2, 1},
+				{2, 2},
+				{2, 3},
+				{3, 1},
+				{3, 2},
+				{3, 3},
+			},
+		},
+		{
+			name: "Two items, m = 3",
+			in:   []int{1, 2},
+			m:    3,
+			out: [][]int{
+				{1},
+				{2},
+				{1, 1},
+				{1, 2},
+				{2, 1},
+				{2, 2},
+				{1, 1, 1},
+				{1, 1, 2},
+				{1, 2, 1},
+				{1, 2, 2},
+				{2, 1, 1},
+				{2, 1, 2},
+				{2, 2, 1},
+				{2, 2, 2},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			out := AllRepeat(tc.in, tc.m)
+			if !reflect.DeepEqual(out, tc.out) {
+				t.Errorf("error: \nreturn:\t%v\nwant:\t%v", out, tc.out)
+			}
+		})
+	}
+}
